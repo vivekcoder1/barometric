30b902b9b982387f624d9dc22d815d82109d84fd
vivekcoder1 <vivek.ravinutala@gmail.com>
2025-09-17 21:47:47 -0400

    several optimizations made

--- barometric_pid.cpp
+++ barometric_pid.cpp
@@ -2,36 +2,41 @@
 #include <Adafruit_BMP085.h>
 bool measuredBasePressure = false;
 bool BMPconnected = false;
+// All constants cached rather than recomputed every call of findheight()
+//reference tempertature at sea level in kelvin
+constexpr float T0 = 288.15;
+//temperature lapse rate in kelvin/m
+constexpr float L = 0.0065;
+//molar mass of air in KJ/mol
+constexpr float M = 0.0289644;
+//universal gas constant in J/mol*K
+constexpr float R = 8.3144598;
+//gravity in m/s^2
+constexpr float g = 9.8;
 
+constexpr float exponent = (R * L) / (M * g);
 
 Adafruit_BMP085 BMP;
 float heightNow = 0;
 float height = 0;
 float initialPressure;
-float confidence = 0.05;
+constexpr float confidence = 0.05;
 
 
 void setup() {
     Serial.begin(9600);
-    BMPconnected = BMP.begin();
 
-
-    if (BMPconnected)
-    {
-        Serial.println("BMP180 found and connected.");
-    }
-    else
-    {
-        Serial.println("BMP180 not found! Check connections.");
-    }
+    //I'm guessing BMP.begin() is bool
+    if (BMP.begin()) Serial.println("BMP180 found and connected.");
+    else Serial.println("BMP180 not found! Check connections.");
 }
 
 
 void loop() {
-    if (measuredBasePressure == false)
+    if (!measuredBasePressure) 
     {
         Serial.println("Place device at base and type go.");
-        while (Serial.available() == 0)
+        while (!Serial.available())
         {
             //wait
         }
@@ -42,7 +47,7 @@ void loop() {
     {
         heightNow = findHeight(initialPressure);
         //low pass filter smoothening
-        height = height * (1 - confidence) + confidence * heightNow;
+        height += confidence * (heightnow - height);
         //-3 and 3 are the range of graph I want to display. The graph will also display the height and heightNow lines.
         Serial.print(-3);
         Serial.print(",");
@@ -61,31 +66,14 @@ void loop() {
 
 
 //barometric formula for height: h = (T0/L)*(1-(Ph/P0)^((R*L)/(M*g)))
-/**
-*Returns the current height of the rocket compared to the reference point in feet
-*/
+/*Returns the current height of the rocket compared to the reference point in feet */
 float findHeight(float initialPressure)
 {
-    //reference tempertature at sea level in kelvin
-    float T0 = 288.15;
-    //temperature lapse rate in kelvin/m
-    float L = 0.0065;
-    //molar mass of air in KJ/mol
-    float M = 0.0289644;
-    //universal gas constant in J/mol*K
-    float R = 8.3144598;
-    //base pressure in pascals
-    float P0 = initialPressure;
-    //pressure at height in pascals
     float Ph = BMP.readPressure();
     //gravity in m/s^2
-    float g = 9.8;
-    //height above base in meters
-    float height;
+    float P0 = initialPressure;
 
-
-    float exponent = (R * L) / (M * g);
-    height = (T0 / L) * (1 - pow((Ph / P0), exponent));
+    float height = (T0 / L) * (1 - pow((Ph / P0), exponent));
     //convert to feet
     height *= 3.28084;
     return height;
